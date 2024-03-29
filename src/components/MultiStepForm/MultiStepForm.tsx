99fd0f98943e64131b4d6b2b8b75314b50c9899e
Fahad Ahmed <fahad.ahmed@me.com>
2024-03-30 10:31:21 +1100

    updated styles to the ui elements

--- src/components/MultiStepForm/MultiStepForm.tsx
+++ src/components/MultiStepForm/MultiStepForm.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { useFormStore } from "../stores/useFormStore"
+import "./MultiStepForm.css"
 
 type FormConfigType = {
   stepName: string
@@ -11,20 +12,11 @@ export type MultiStepFormProps = {
   formConfig: FormConfigType[]
 }
 
-// 1. Get the formConfig data as props from the parent component
-//         2. Create a state variable to store the current step
-//         3. Create a function to handle the next step
-//         4. Create a function to handle the previous step
-//         5. Create a function to handle the form submission
-//         6. Render the form steps based on the current step
-//         7. Add a button to navigate to the next step
-//         8. Add a button to navigate to the previous step
-//         9. Add a button to submit the form
 export default function MultiStepForm({ formConfig }: Readonly<MultiStepFormProps>) {
   const { currentStep, setCurrentStep } = useFormStore()
 
   return (
-    <div style={{ display: 'grid', gridTemplateColumns: '3fr 1fr', width: '100%', height: '100%', gap: '2rem' }}>
+    <div className="form-container">
       <div>
         {formConfig.map((stepData, index) => {
           return (
@@ -40,12 +32,11 @@ export default function MultiStepForm({ formConfig }: Readonly<MultiStepFormProp
           )
         })}
       </div>
-      <div style={{ background: "#efefef", padding: '2rem' }}>
+      <div className="sidebar">
         <h3>On this page</h3>
         <ul>
           {formConfig.map((stepData, index) => (
             <li key={index} style={{ marginBottom: '1rem' }} onClick={() => setCurrentStep(index)}>
-              {/* <button onClick={() => setStep(index)}></button> */}
               {currentStep === index ? <strong>{stepData.stepName}</strong> : stepData.stepName}
             </li>
           ))}
--- src/components/forms/PersonalDetailsForm/PersonalDetailsForm.tsx
+++ src/components/forms/PersonalDetailsForm/PersonalDetailsForm.tsx
@@ -2,6 +2,7 @@ import { useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useFormStore } from "../../stores/useFormStore"
 import personalDetailsSchema from "./personalDetailsSchema"
+import '../../../App.css';
 
 export default function PersonalDetailsForm() {
   const { currentStep, setCurrentStep } = useFormStore()
@@ -26,53 +27,57 @@ export default function PersonalDetailsForm() {
 
   return (
     <div>
-      <h3>Personal Details</h3>
-      <div>This is where we show the form</div>
-      {currentStep === thisStep ? (
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr', gap: '2rem' }}>
-            <div style={{ display: 'grid' }}>
-              <label htmlFor="firstName">First Name</label>
-              {errors?.firstName && <small>{errors?.firstName?.message as string}</small>}
-              <input type="text" placeholder="" id="firstName" {...register('firstName')} />
-            </div>
-            <div style={{ display: 'grid' }}>
-              <label htmlFor="lastName">Last Name</label>
-              {errors?.lastName && <small>{errors?.lastName?.message as string}</small>}
-              <input type="text" placeholder="" id="lastName" {...register('lastName')} />
-            </div>
-            <div style={{ display: 'grid' }}>
-              <label htmlFor="gender">Gender</label>
-              {errors?.gender && <small>{errors?.gender?.message as string}</small>}
-              <select id="gender" {...register('gender')}>
-                <option value="">Select</option>
-                <option value="MALE">Male</option>
-                <option value="FEMALE">Female</option>
-              </select>
-
-            </div>
-            <div style={{ display: 'grid' }}>
-              <label htmlFor="staffType">Role</label>
-              {errors?.staffType && <small>{errors?.staffType?.message as string}</small>}
-              <select id="staffType" {...register('staffType')}>
-                <option value="">Select</option>
-                <option value="TEACHER">Teacher</option>
-                <option value="EDUCATOR">Educator</option>
-              </select>
-
-            </div>
-            <div style={{ display: 'grid' }}>
-              <label htmlFor="vitNumber">VIT number</label>
-              {errors?.vitNumber && <small>{errors?.vitNumber?.message as string}</small>}
-              <input type="text" placeholder="" id="vitNumber" {...register('vitNumber')} />
-            </div>
+      <h3 style={{ color: currentStep !== thisStep ? '#000' : "#87189D" }}>Personal Details</h3>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr', gap: '2rem' }}>
+          <div style={{ display: 'grid' }}>
+            <label htmlFor="firstName">First Name</label>
+            {errors?.firstName && <small>{errors?.firstName?.message as string}</small>}
+            <input
+              type="text"
+              placeholder=""
+              id="firstName"
+              {...register('firstName')}
+              disabled={currentStep !== thisStep}
+              className="input-field"
+            />
           </div>
-          <div style={{ display: 'flex', justifyContent: 'end', alignItems: 'center', gap: '1rem' }}>
-            <button type="reset">Discard</button>
-            <button type="submit">Continue</button>
+          <div style={{ display: 'grid' }}>
+            <label htmlFor="lastName">Last Name</label>
+            {errors?.lastName && <small>{errors?.lastName?.message as string}</small>}
+            <input type="text" placeholder="" id="lastName" {...register('lastName')} disabled={currentStep !== thisStep} className="input-field" />
           </div>
-        </form>
-      ) : <div>Form fields are hidden</div>}
+          <div style={{ display: 'grid' }}>
+            <label htmlFor="gender">Gender</label>
+            {errors?.gender && <small>{errors?.gender?.message as string}</small>}
+            <select id="gender" {...register('gender')} disabled={currentStep !== thisStep}>
+              <option value="">Select</option>
+              <option value="MALE">Male</option>
+              <option value="FEMALE">Female</option>
+            </select>
+
+          </div>
+          <div style={{ display: 'grid' }}>
+            <label htmlFor="staffType">Role</label>
+            {errors?.staffType && <small>{errors?.staffType?.message as string}</small>}
+            <select id="staffType" {...register('staffType')} disabled={currentStep !== thisStep}>
+              <option value="">Select</option>
+              <option value="TEACHER">Teacher</option>
+              <option value="EDUCATOR">Educator</option>
+            </select>
+
+          </div>
+          <div style={{ display: 'grid' }}>
+            <label htmlFor="vitNumber">VIT number</label>
+            {errors?.vitNumber && <small>{errors?.vitNumber?.message as string}</small>}
+            <input type="text" placeholder="" id="vitNumber" {...register('vitNumber')} disabled={currentStep !== thisStep} className="input-field" />
+          </div>
+        </div>
+        <div style={{ display: currentStep !== thisStep ? 'none' : 'flex', justifyContent: 'end', alignItems: 'center', gap: '1rem' }}>
+          <button type="reset">Discard</button>
+          <button type="submit">Continue</button>
+        </div>
+      </form>
     </div>
   )
 }
